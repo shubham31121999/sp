7ed862c0ae3632ff73cead4df129de743a1b4918
Shubham Gajanan Uparkar <65241730+shubham31121999@users.noreply.github.com>
2025-07-08 15:00:18 +0530

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/mainClient/out',
+//   assetPrefix: '/mainClient/out',
+//   publicRuntimeConfig: {
+//     assetPrefix: '/mainClient/out',
+//   },
+//   images: {
+//     unoptimized: true, // ✅ THIS IS REQUIRED for static export!
+//   },
+// };
+
+// export default nextConfig;
+// export const { publicRuntimeConfig: { assetPrefix } } = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/mainClient/out',
-  assetPrefix: '/mainClient/out',
-  publicRuntimeConfig: {
-    assetPrefix: '/mainClient/out',
-  },
-  images: {
-    unoptimized: true, // ✅ THIS IS REQUIRED for static export!
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
-export const { publicRuntimeConfig: { assetPrefix } } = nextConfig;
